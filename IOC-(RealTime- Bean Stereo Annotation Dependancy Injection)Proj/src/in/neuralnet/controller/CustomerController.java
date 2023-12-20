654bc04276effa41aa7faefb1aba39d9e0e0cc16
INDRA <89997443+ZeusSR@users.noreply.github.com>
2023-12-20 16:29:28 +0530

    Add files via upload

---
+++ IOC-(RealTime- Bean Stereo Annotation Dependancy Injection)Proj/src/in/neuralnet/controller/CustomerController.java
@@ -0,0 +1,35 @@
+package in.neuralnet.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import in.neuralnet.dto.CustomerDTO;
+import in.neuralnet.service.ICustomerMgmtService;
+import in.neuralnet.vo.CustomerVo;
+
+@Component("controller")
+public class CustomerController {
+
+	@Autowired
+	ICustomerMgmtService service;
+
+	static {
+		System.out.println("Controller.class file is loading...");
+	}
+
+	
+
+	public String processResult(CustomerVo vo) {
+		CustomerDTO dto = new CustomerDTO();
+		
+		dto.setCustomerName(vo.getCustomerName());
+		dto.setCustomerAddress(vo.getCustomerAddress());
+		dto.setPamt(Float.parseFloat(vo.getPamt()));
+		dto.setRate(Float.parseFloat(vo.getRate()));
+		dto.setTime(Float.parseFloat(vo.getTime()));
+		
+		
+		String result = service.calculateSimpleInterest(dto);
+		return result;
+	}
+}
---
+++ IOC-(RealTime- Bean Stereo Annotation Dependancy Injection)Proj/src/in/neuralnet/dao/CustomerPGSQLDAOImp.java
@@ -0,0 +1,64 @@
+package in.neuralnet.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import in.neuralnet.BO.CustomerBO;
+
+@Repository("dao")
+public class CustomerPGSQLDAOImp implements ICustomerDAO {
+
+	static
+	{
+		System.out.println("CustomerPGSQLDAOImp.class Loading..");
+	}
+	int res = 0;
+	private static final String CUSTOMER_INSERT_QUERY = "INSERT INTO public.\"SPRING_CUSTOMER\"(\r\n"
+		+" \"CNAME\", \"CADDR\", \"PAMNT\", \"INTRAMNT\")\r\n"
+			+ "	VALUES (?, ?, ?, ?);";
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	
+	
+	public CustomerPGSQLDAOImp(DataSource dataSource) {
+		super();
+		
+		System.out.println("CustomerPGSQLDAOImp: 1 parm constructor.....");
+		this.dataSource = dataSource;
+	}
+
+
+
+	@Override
+	public int insert(CustomerBO bo) {
+
+		try (Connection connection = dataSource.getConnection()) {
+			
+			System.out.println(bo);
+			
+			PreparedStatement pstmt = connection.prepareStatement(CUSTOMER_INSERT_QUERY);
+			
+			pstmt.setString(1, bo.getCustomerName());
+			pstmt.setString(2, bo.getCustomerAddress());
+			pstmt.setFloat(3, bo.getPamt());
+			pstmt.setFloat(4, bo.getInterestAmt());
+			
+			 res = pstmt.executeUpdate();
+		
+			
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+		return res;
+	}
+
+}
